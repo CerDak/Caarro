715dfebae1cf51941f0002560a897ebb5ccca127
Dakota Clark <dakota@allclarks.com>
2023-03-25 19:13:25 -0700

    feat: add services service
    CRUDL API for car servicing events.

---
+++ Caarro/Services/ServicesService.cs
@@ -0,0 +1,52 @@
+using Caarro.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace Caarro.Services;
+
+public class ServicesService
+{
+    private ILogger<ServicesService> _log;
+    private readonly CaarroDbContext _db;
+
+    public ServicesService(ILogger<ServicesService> logger, CaarroDbContext db)
+    {
+        _log = logger;
+        _db = db;
+    }
+
+    public async Task<List<Service?>> GetAllServicesAsync(int carId)
+    {
+        return (await _db.Services.Where(s => s.VehicleId == carId && s.Active == true).ToListAsync())!;
+    }
+
+    public async Task<Service?> GetServiceAsync(int id)
+    {
+        return await _db.Services.SingleOrDefaultAsync(s => s.Id == id);
+    }
+
+    public async Task AddServiceAsync(Service service)
+    {
+        service.Date = DateTime.Now;
+        service.Active = true;
+
+        _db.Services.Add(service);
+        await _db.SaveChangesAsync();
+    }
+
+    public async Task DeleteServiceAsync(Service service)
+    {
+        var x = await _db.Services.SingleOrDefaultAsync(s => s.Id == service.Id);
+        if (x is not null)
+        {
+            x.Active = false;
+            _db.Services.Update(x);
+            await _db.SaveChangesAsync();
+        }
+    }
+
+    public async Task UpdateServiceAsync(Service service)
+    {
+        _db.Services.Update(service);
+        await _db.SaveChangesAsync();
+    }
+}
