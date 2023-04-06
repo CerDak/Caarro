c75cc5d82588627b4e00ac4384105a43a7d39e59
Dakota Clark <dakota@allclarks.com>
2023-04-05 21:32:38 -0700

    feat: add migration task to the build script

--- .build.ps1
+++ .build.ps1
@@ -1,5 +1,20 @@
+param(
+    [ValidateNotNullOrEmpty()]
+    [string]$MigrationName
+)
+
 task . {
     exec { podman build -f .\Caarro\Dockerfile -t registry.fly.io/caarro:latest . }
     exec { podman push registry.fly.io/caarro:latest }
     exec { flyctl deploy }
+}
+
+task migrate {
+    if ($null -ne $MigrationName) {
+        $MigrationName = $MigrationName.Replace(' ', '_')
+        cd .\Caarro
+        exec { dotnet ef migrations add $MigrationName }
+    } else {
+        Write-Error 'Set the parameter -MigrationName to use the migration task' -EA Stop
+    }
 }
