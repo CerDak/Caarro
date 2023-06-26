1658ecb48790ff3eb6c2bbd8f54e012ad092ac87
Dakota Clark <dakota@allclarks.com>
2023-06-25 18:58:35 -0700

    refactor: migrate just prints command
    I don't really need to redo this in ib.
    Just telling me how to do it again is enough.

--- .build.ps1
+++ .build.ps1
@@ -1,8 +1,3 @@
-param(
-    [ValidateNotNullOrEmpty()]
-    [string]$MigrationName
-)
-
 task . {
     exec { podman build -f .\Caarro\Dockerfile -t registry.fly.io/caarro:latest . }
     exec { podman push registry.fly.io/caarro:latest }
@@ -10,11 +5,5 @@ task . {
 }
 
 task migrate {
-    if ($null -ne $MigrationName) {
-        $MigrationName = $MigrationName.Replace(' ', '_')
-        cd .\Caarro
-        exec { dotnet ef migrations add $MigrationName }
-    } else {
-        Write-Error 'Set the parameter -MigrationName to use the migration task' -EA Stop
-    }
+    Write-Output -Message 'Run a migraiton with "dotnet ef migraitons add $Name"'
 }
